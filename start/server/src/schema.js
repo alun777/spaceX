1ca73ea8e5ebbfa3af70ae488724877eed0e70ec
ChaoYIN <yinchao428@hotmail.com>
2020-01-19 15:40:34 +1100

    update mutation in schema

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -11,8 +11,36 @@ const typeDefs = gql`
     id: ID!
     site: String
     mission: Mission
-    rocket: rocket
+    rocket: Rocket
     isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String #login token
   }
 `;
 
